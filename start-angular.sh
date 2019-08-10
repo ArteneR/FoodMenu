541a31dbaad1ca0844b48040042edc15d9076c7f
ArteneR <georgevici.ioan@yahoo.com>
2019-08-11 01:45:46 +0300

    Added scripts for running angular and laravel

---
+++ start-angular.sh
@@ -0,0 +1,7 @@
+
+echo "Starting Angular... Please go to localhost:4200 once Angular has started"
+echo $'\n'
+
+cd angular-module
+
+ng serve
---
+++ start-laravel.sh
@@ -0,0 +1,7 @@
+
+echo "Starting Laravel... Please go to localhost:8000 once Laravel has started"
+echo $'\n'
+
+cd laravel-module
+
+php artisan serve
