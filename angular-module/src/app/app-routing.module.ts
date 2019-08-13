333f173b074cd68840a86d310146c8b3886db818
ArteneR <georgevici.ioan@yahoo.com>
2019-08-14 00:35:28 +0300

    Made the page not found 404 route

--- angular-module/src/app/app-routing.module.ts
+++ angular-module/src/app/app-routing.module.ts
@@ -1,27 +1,36 @@
 import { NgModule }             from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
     {
+        // Admin area
+        path: 'admin',
+        loadChildren: () => import('./admin/admin.module').then(mod => mod.AdminModule)
+    },
+    {
+        // Website area
         path: '',
         loadChildren: () => import('./website/website.module').then(mod => mod.WebsiteModule)
     },
     {
-        path: 'admin',
-        loadChildren: () => import('./admin/admin.module').then(mod => mod.AdminModule)
-    },
-//    {
-//        // Page not found
-//        path: '**',
-//        loadChildren: () => import('./admin/admin.module').then(mod => mod.AdminModule)
-//    }
+        // Page not found
+        path: '**',
+        component: PageNotFoundComponent
+    }
 ];
 
 
 @NgModule({
+    declarations: [
+        PageNotFoundComponent
+    ],
     imports: [
-        RouterModule.forRoot(routes)
+        RouterModule.forRoot(
+            routes,
+            { enableTracing: false } // <-- debugging purposes only
+        )
     ],
     exports: [
         RouterModule
